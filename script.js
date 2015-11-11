ee08c4aeabf82ad9f12b2e8a928f75cd5608478a
Blake DeBaske <blakedebaske@gmail.com>
2015-11-11 09:08:36 -0500

    updated css

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+// I had a non a few of the objectives working for this lab and few that were not working. 
+// I sat with a few people that were in the group that you led to go over what was discussed
+// and copied their code. I somehow locked myself out of my initial js file via get"admin right," but I will
+// work on getting that unlocked
+
+
 $(document).ready(function(){
 
 	$("li").hover(function(){
